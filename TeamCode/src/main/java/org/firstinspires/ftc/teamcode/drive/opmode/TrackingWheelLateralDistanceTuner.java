b03ee1c1eb529db253b1cca8f1cdf4d99cb45d18
JIceberg <jisenberg3@gatech.edu>
2020-12-19 22:09:23 -0500

    revert
    This reverts commit 10cf75975815a2ca590dd7ac7aeb92dc6dd2556c, reversing
    changes made to 993fae931604e8fcf6423f96b3e6a1a58264f595.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TrackingWheelLateralDistanceTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TrackingWheelLateralDistanceTuner.java
@@ -10,7 +10,6 @@ import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.RobotLog;
 
